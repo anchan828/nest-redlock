9bfe33499f419d559aa0e2ebec2d83c106149ca4
anchan828 <anchan828@gmail.com>
2022-11-28 22:40:23 +0900

    feat: add decoratorHooks

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 export { Redlock } from "./redlock.decorator";
 export { FakeRedlockService } from "./redlock.fake-service";
-export { RedlockKeyFunction, RedlockModuleOptions } from "./redlock.interface";
+export {
+  LockedKeysHookArgs,
+  PreLockedKeysHookArgs,
+  RedlockKeyFunction,
+  RedlockModuleOptions,
+  UnlockedKeysHookArgs,
+} from "./redlock.interface";
 export { RedlockModule } from "./redlock.module";
 export { RedlockService } from "./redlock.service";
