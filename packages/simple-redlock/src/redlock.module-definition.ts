651825450a149698e4f8c8a98d3009019a97eaac
anchan828 <anchan828@gmail.com>
2024-01-12 11:33:19 +0900

    chore: remove simple-redlock package

--- packages/simple-redlock/src/redlock.module-definition.ts
+++
@@ -1,5 +0,0 @@
-import { ConfigurableModuleBuilder } from "@nestjs/common";
-import { SimpleRedlockModuleOptions } from "./redlock.interface";
-
-export const { ConfigurableModuleClass, MODULE_OPTIONS_TOKEN } =
-  new ConfigurableModuleBuilder<SimpleRedlockModuleOptions>().build();
